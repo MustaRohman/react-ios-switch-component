23b117fbfb770c6f9be46a6c69ea822ddcf2df3c
Musta Rohman <mustarohman@Mustas-MBP.home>
2019-02-07 20:13:56 +0000

    initial component

---
+++ src/ios-switch-Component/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./index.css";
+
+export class IosSwitchComponent extends React.Component{
+    render() {
+        return (
+            <div className="container">
+                <div className="inner-circle"></div>
+            </div>
+        )
+    }
+}
